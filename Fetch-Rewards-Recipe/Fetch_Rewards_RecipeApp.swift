5d4292e57fbc563f479e539777414251371a25cb
Keerthana Srinivasan <keerks07@gmail.com>
2024-06-12 11:28:48 -0400

    Initial Commit

---
+++ Fetch-Rewards-Recipe/Fetch_Rewards_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Rewards_RecipeApp.swift
+//  Fetch-Rewards-Recipe
+//
+//  Created by Keerthana Srinivasan on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Rewards_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
